6ebe323af028a846477a3f204fbab9a2a5c9fb8b
MatteoFalcioni <64528038+MatteoFalcioni@users.noreply.github.com>
2022-02-11 16:41:08 +0100

    Add files via upload

--- kuramoto.cpp
+++ kuramoto.cpp
@@ -5,7 +5,6 @@ using namespace std::complex_literals;
 static int N;
 
 //definire i parametri di campo medio
-
 void Oscillator::MeanField() {
 
 std::vector<Oscillator> System(N); //insieme degli oscillatori
@@ -16,10 +15,10 @@ for(int i=0; i<=N; ++i){
     COS += std::cos(System[i].theta_);  //COS è la somma di tutti i coseni di theta_i
     SIN += std::cos(System[i].theta_);  //stesso per SIN
 }
-double r = std::sqrt(COS*COS + SIN*SIN);   //possiamo vedere la somma dei fasori come COS + iSIN = X + iY. Da qui lo riportiamo in exp
-double psi = std::atan(SIN/COS);
-std::complex<double> MF = std::polar(r, psi);
-
-std::complex<double> Z;  //Z è il parametro d'ordine, cioè X rinormalizzato a N. Deve essere espresso come re^i*Psi però
+double X = COS/N;
+double Y = SIN/N;
+double r = std::sqrt(X*X + Y*Y);   //possiamo vedere la somma dei fasori come COS/N + i*SIN/N = X + iY. Da qui lo riportiamo in exp
+double psi = std::atan(Y/X); //non è così semplice calcolare la fase, non so se esiste qualche funzipme che considera i vari casi possibili
+std::complex<double> MF = std::polar(r, psi);  //polar costruisce un exp complesso, MF sta per mean field non per Matteo Falcioni
 
 }
