bcad9e3743d58009696cd338e6bea1e4ebc19151
puccj <daniele.pucci2@studio.unibo.it>
2022-02-11 20:17:36 +0100

    Iniziata la grafica in Ascii

--- kuramoto.h
+++ kuramoto.h
@@ -9,16 +9,22 @@ using namespace std::complex_literals;
 
 class Oscillator{
   //static int _N;   //# of oscillator
+  
+  static int _gridDim;  //dimension (side) of the grid where oscillators will be placed
+  int _position;        //position of oscillator on the grid
+
   //static double _K; //parametro di accoppiamento (in inglese come?)
   double _freq;   //frequency omega
   double _phase;  //phase phi/theta
   
+  static void setDefaultGridDim() { _gridDim = 10; }
   std::complex<double> phasor() { return std::exp(1i*_phase); }  //returns directly the exponential form
 
  public:
   //if not indicated, frequencies and phases are set random
-  Oscillator(double freq = -1, double phase = -1);
+  Oscillator(double freq = -1, double phase = -1, double position = -1);
   
+  static double setGridDim(int gridDim) { _gridDim = gridDim; }
   double freq() { return _freq; }
   void setFreq(double freq) { _freq = freq; }
   double phase() { return _phase; };
@@ -26,6 +32,9 @@ class Oscillator{
   
   //return r: parameter of order
   static std::complex<double> orderParamether(std::vector<Oscillator>& system);
+  
+  //print system in ascii
+  static void print(std::vector<Oscillator>& system);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main(){
   int N = 10;
+  Oscillator::setGridDim(5);
   std::vector<Oscillator> system(N);  //create an array of N oscillator (no parameter set -> random)
-  
   
 }
