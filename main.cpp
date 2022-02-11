65ef2fda47e6915d217113b696be171be6030409
puccj <daniele.pucci2@studio.unibo.it>
2022-02-11 23:55:36 +0100

    Grafica basic con solo la posizione

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-#include "kuramoto.h"
+#include "oscillator.h"
 
 int main(){
   int N = 10;
   Oscillator::setGridDim(5);
   std::vector<Oscillator> system(N);  //create an array of N oscillator (no parameter set -> random)
+  Oscillator::print(system);
   
 }
