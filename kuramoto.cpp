d215d38aa11c35bd277520fb83d1082ada5def8c
MatteoFalcioni <64528038+MatteoFalcioni@users.noreply.github.com>
2022-02-11 16:10:24 +0100

    Add files via upload

--- kuramoto.cpp
+++ kuramoto.cpp
@@ -1,2 +1,17 @@
 #include "kuramoto.h"
+#include <vector>
 
+static int N;
+
+//definire i parametri di campo medio
+
+void Oscillator::MeanField() {
+
+std::vector<Oscillator> System(N); //insieme degli oscillatori
+std::complex<double> X = 0;
+for(int i=0; i<=N; ++i){
+    X += System[i].phasor;  //X è la somma di tutti gli esponenziali con fase theta_i
+}
+std::complex<double> Z = (1/N)*X;  //Z è il parametro d'ordine, cioè X rinormalizzato a N. Deve essere espresso come re^i*Psi però
+
+}
