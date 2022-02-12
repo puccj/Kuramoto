2a60d335d13b80e27984ab3fd4a2e08e8b1ecd44
MatteoFalcioni <64528038+MatteoFalcioni@users.noreply.github.com>
2022-02-12 12:06:30 +0100

    Update oscillator.h

--- oscillator.h
+++ oscillator.h
@@ -9,7 +9,7 @@ using namespace std::complex_literals;
 
 
 class Oscillator{
-  //static double _K; //parametro di accoppiamento (in inglese come?)
+  //static double _K; //coupling strength 
   static double _dt;
   double _freq;   //frequency omega
   double _phase;  //phase phi/theta
@@ -17,7 +17,7 @@ class Oscillator{
   bool phaseNearZero(double limit = _dt*2) { return (_phase < limit || _phase > 2*M_PI - limit); }
   static void setDefaultDt() { _dt = 0.1; }
   std::complex<double> phasor() { return std::exp(1i*_phase); }                 //returns directly the exponential form
-  static std::complex<double> orderParamether(std::vector<Oscillator>& system); //returns r: parameter of order
+  static std::complex<double> orderParamether(std::vector<Oscillator>& system); //returns r: order parameter
 
  public:
   //if not indicated, frequencies and phases are set randomly
@@ -31,7 +31,7 @@ class Oscillator{
   
   //print the oscillator in ascii
   void print();
-  //evolve the dinamic of the oscillator
+  //evolve the oscillator's dinamic
   void evolve();
 };
 
