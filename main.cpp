20ebcc4c24bb2e1442d3fa5661e45c49cb35fdc6
puccj <daniele.pucci2@studio.unibo.it>
2022-03-17 12:15:54 +0100

    Pulito il main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "swarm.h"
 #include <thread>
 #include <iostream>
-
 //#include <X11/Xlib.h> 
 
 void draw(Swarm& syst) {
@@ -17,26 +16,21 @@ void evolve(Swarm& syst, bool saveData) {
 int main() {
   //XInitThreads();  //serve per XServer
 
-  //Swarm lorRK(1000, Distribution{DistName::Lorentz, 1, 0.5}); //array con distribuzione di Lorentz a media 1 e gamma 0.5
-  //Swarm primo(1000, 0.5, TitledWindow{"Esempio"});  //array con tutti alla stessa frequenza di 0.5Hz
-  //primo.setK(0.1);
-  //primo.setWindowDim(1920,1080);
+  Swarm esempio(1000, Distribution{DistName::Lorentz, 1, 0.5}, {"Esempio", 1280, 720}); //array con distribuzione di Lorentz a media 1 e gamma 0.5
+  esempio.setK(10);
 
-  Swarm expo(1000, Distribution{DistName::Exp, 2});
-  //expo.setK(20);
   //Draw window, plot window and evolve done in separeted threads
-  //std::thread primoDrawThr(&draw, std::ref(expo));
-  //std::thread primoPlotThr(&plot, std::ref(expo));
-  //std::thread primoEvolveThr(&evolve, std::ref(expo), true);
+  std::thread drawThr(&draw, std::ref(esempio));
+  std::thread plotThr(&plot, std::ref(esempio));
+  std::thread evolveThr(&evolve, std::ref(esempio), false);
 
   //wait for threads to terminate before proceding
-  //primoDrawThr.join(); 
-  //primoPlotThr.join();
-  //expo.stopEvolve();
-  //primoEvolveThr.join();
+  drawThr.join(); 
+  plotThr.join();
+  esempio.stopEvolve();
+  evolveThr.join();
 
-  expo.rkGraph(10,20,1, true);
-  //lorRK.rkGraph(18, 23, 1, true, 0.003);
+  //esempio.rkGraph(10,20,1, true);
 
   return 0;
 }
